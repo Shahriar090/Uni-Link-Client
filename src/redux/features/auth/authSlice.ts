9992ce384bb79722cdd0e7ecfc4d255fc2ad7f65
Shahriar <shahriarh655@gmail.com>
2024-09-01 13:55:48 +0600

    Implemented user logout

--- src/redux/features/auth/authSlice.ts
+++ src/redux/features/auth/authSlice.ts
@@ -17,7 +17,6 @@ const authSlice = createSlice({
   reducers: {
     setUser: (state, action) => {
       const { user, token } = action.payload;
-      console.log("User from slice", user);
       state.user = user;
       state.token = token;
     },
