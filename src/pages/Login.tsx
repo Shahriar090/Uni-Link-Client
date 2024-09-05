898a3da98c739fe8fe7f41464ed1a64f6d32898f
Shahriar <shahriarh655@gmail.com>
2024-09-05 20:46:42 +0600

    Configuring forms with react hook form's form provider

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -6,11 +6,12 @@ import { setUser, TUser } from "../redux/features/auth/authSlice";
 import { verifyToken } from "../utils/verifyToken";
 import { useNavigate } from "react-router-dom";
 import { toast } from "sonner";
+import PhForm from "../components/form/PhForm";
 
 const Login = () => {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
-  const { register, handleSubmit } = useForm({
+  const { register } = useForm({
     defaultValues: {
       id: "A-0001",
       password: "admin123",
@@ -19,20 +20,21 @@ const Login = () => {
   const [login] = useLoginMutation();
 
   const onsubmit = async (data: FieldValues) => {
-    const toastId = toast.loading("Logging IN", { duration: 3000 });
-    try {
-      const userInfo = {
-        id: data.id,
-        password: data.password,
-      };
-      const res = await login(userInfo).unwrap();
-      const user = verifyToken(res.data.accessToken) as TUser;
-      dispatch(setUser({ user: user, token: res.data.accessToken }));
-      navigate(`/${user.role}/dashboard`);
-      toast.success("User Login Successful", { duration: 3000, id: toastId });
-    } catch (err) {
-      toast.error("User Login Failed", { duration: 3000, id: toastId });
-    }
+    console.log(data);
+    // const toastId = toast.loading("Logging IN", { duration: 3000 });
+    // try {
+    //   const userInfo = {
+    //     id: data.id,
+    //     password: data.password,
+    //   };
+    //   const res = await login(userInfo).unwrap();
+    //   const user = verifyToken(res.data.accessToken) as TUser;
+    //   dispatch(setUser({ user: user, token: res.data.accessToken }));
+    //   navigate(`/${user.role}/dashboard`);
+    //   toast.success("User Login Successful", { duration: 3000, id: toastId });
+    // } catch (err) {
+    //   toast.error("User Login Failed", { duration: 3000, id: toastId });
+    // }
   };
   return (
     <div
@@ -40,7 +42,7 @@ const Login = () => {
     >
       <h1>Login Please</h1>
       <div>
-        <form onSubmit={handleSubmit(onsubmit)}>
+        <PhForm onSubmit={onsubmit}>
           <div>
             <label htmlFor="id">ID:</label>
             <input type="text" id="id" {...register("id")} />
@@ -50,7 +52,7 @@ const Login = () => {
             <input type="text" id="password" {...register("password")} />
           </div>
           <Button htmlType="submit">Submit</Button>
-        </form>
+        </PhForm>
       </div>
     </div>
   );
